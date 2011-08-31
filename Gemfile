31eddf60ee465be1e75956906fee5218d538c97d
Peter Kordel <pkordel@gmail.com>
2011-08-31 20:50:38 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'pg'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'heroku'
+  gem 'sqlite3'
+  gem 'rspec-rails'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nith::Application.config.secret_token = '451bf8a9724357ff6e0ea9481fa1f1d61cee471a804a265055abbe052de04089ffc42523945ccde2cf29b5b5c7d216c3758336684f6eb6714924630053283adb'
